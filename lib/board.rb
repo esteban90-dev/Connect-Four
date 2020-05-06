4044cd0e71c59b2c0284529c2647da7378757d78
Stephen Allard <stephencallard@gmail.com>
2020-05-06 03:33:06 -0500

    Removed #get_element - this is handled by #grid

--- lib/board.rb
+++ lib/board.rb
@@ -5,10 +5,6 @@ class Board
     @grid = input.fetch(:grid, default_grid)
   end
 
-  def get_element(column, element)
-    grid[column][element]
-  end
-
   def push(column, value)
     i = 0
     while i < grid[column].length
