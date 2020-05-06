e5f411ff2b09dc16eaf688a481c08bc5dfc66061
Stephen Allard <stephencallard@gmail.com>
2020-05-06 03:06:33 -0500

    Added additional test for #initialize

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -7,27 +7,18 @@ describe Board do
       expect(board1.grid.size).to eql(7)
     end
 
-    it "creates a grid where each column has 6 things in it by default" do
+    it "creates a grid where each column has a length of 6 by default" do
       board1 = Board.new
       board1.grid.each do |column|
         expect(column.size).to eql(6)
       end
     end
-  end
 
-  context "#get_cell" do
-    it "returns the contents of grid coordinate" do
+    it "creates a grid where each grid coordinate is nil by default" do
       board1 = Board.new
-      board1.push(3,33)
-      expect(board1.get_cell([3,0])).to eql(33)
-    end
-  end
-
-  context "#push" do
-    it "Pushes values into a column of the grid" do
-      board1 = Board.new
-      board1.push(6,55)
-      expect(board1.get_cell([6][0])).to eql(55)
+      board1.grid.each do |column|
+        expect(column.all?{ |value| value.nil? }).to eql(true)
+      end
     end
   end
 
