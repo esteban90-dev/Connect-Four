11e2a907612fb5282ade766e738138ff6d7d090a
Stephen Allard <stephencallard@gmail.com>
2020-05-06 03:59:12 -0500

    Modified tests to account for grid initializing with Cell objects

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -14,10 +14,10 @@ describe Board do
       end
     end
 
-    it "creates a grid where each grid element is nil by default" do
+    it "creates a grid where each grid element's value is "" by default" do
       board1 = Board.new
       board1.grid.each do |column|
-        expect(column.all?{ |element| element.nil? }).to eql(true)
+        expect(column.all?{ |element| element.value == "" }).to eql(true)
       end
     end
   end
@@ -25,34 +25,22 @@ describe Board do
   context "#grid" do
     it "returns the contents of a cell" do
       board1 = Board.new
-      expect(board1.grid[0][0]).to eql(nil)
+      expect(board1.grid[0][0]).not_to eql(nil)
     end
   end
 
   context "#push" do
-
-    TestCell = Struct.new(:name)
-
-    it "Pushes an integer into an empty column" do
+    it "Pushes a character into an empty column" do
       board1 = Board.new
-      board1.push(6,55)
-      expect(board1.grid[6][0]).to eql(55)
+      board1.push(6,"X")
+      expect(board1.grid[6][0].value).to eql("X")
     end
 
-    it "Pushes an object into an empty column" do
+    it "Pushes a character into a partially filled column" do
       board1 = Board.new
-      cell1 = TestCell.new("junk")
-      board1.push(0, cell1)
-      expect(board1.grid[0][0]).to eql(cell1)
-    end
-
-    it "Pushes an object into a partially filled column" do
-      board1 = Board.new
-      cell1 = TestCell.new("junk")
-      cell2 = TestCell.new("junk2")
-      board1.push(0, cell1)
-      board1.push(0, cell2)
-      expect(board1.grid[0][1]).to eql(cell2)
+      board1.push(0, "Z")
+      board1.push(0, "X")
+      expect(board1.grid[0][1].value).to eql("X")
     end
   end
 end
