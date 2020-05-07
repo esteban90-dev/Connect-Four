48b8f05550b0b033a3f39439e14b0e4166fc0a58
Stephen Allard <stephencallard@gmail.com>
2020-05-07 02:44:52 -0500

    Added #formatted, #footer_line_1, and #footer_line_2

--- lib/board.rb
+++ lib/board.rb
@@ -17,11 +17,32 @@ class Board
   end
 
   def formatted
-    
-    
+    grid.transpose.reverse.each do |row|
+      puts row.map{ |element| element.nil? ? " " : element }.join("  |  ")
+    end
+    puts footer_line_1.join("  |  ")
+    puts footer_line_2.join("  |  ")
   end
 
   private
+
+  def footer_line_1
+    footer = []
+    grid.length.times do
+      footer << "-"
+    end
+    footer
+  end
+
+  def footer_line_2
+    i = 0
+    footer = []
+    grid.length.times do
+      footer << "#{i}"
+      i += 1
+    end
+    footer
+  end
 
   def build_grid(columns, height)
     Array.new(columns){ Array.new(height){ Cell.new } }
