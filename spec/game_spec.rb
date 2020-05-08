17b4491d2d1a51f0b904b30a73f78ebc5ca49b92
Stephen Allard <stephencallard@gmail.com>
2020-05-08 03:56:57 -0500

    Added additional #initialize tests

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,7 +4,7 @@ describe Game do
 
   TestPlayer = Struct.new(:name, :color)
   let (:sean) { TestPlayer.new( { :name => "sean", :color => "X"} ) }
-  let (:alex) { TestPlayer.new( { :name => "john", :color => "O"} ) }
+  let (:alex) { TestPlayer.new( { :name => "alex", :color => "O"} ) }
   board1 = Array.new(6){ Array.new(7) }
 
   context "#initialize" do
@@ -14,9 +14,24 @@ describe Game do
     
     it "Does not raise an exception when initialized with a valid input {}" do
       players = [sean, alex]
-      expect{ Game.new({ :players => players, :board =>  board1 }) }.not_to raise_error(KeyError)
+      expect{ Game.new({ :players => :players, :board =>  board1 }) }.not_to raise_error(KeyError)
     end
-  end
 
+    it "Randomly assigns the current player" do
+      players = [sean, alex]
+      allow(players).to receive(:shuffle).and_return([sean,alex])
+      game1 = Game.new({ :players => players, :board =>  board1 })
+      expect(game1.current_player).to eql(sean)
+    end
+
+    it "Randomly assigns the other player" do
+      players = [sean, alex]
+      allow(players).to receive(:shuffle).and_return([sean,alex])
+      game1 = Game.new({ :players => players, :board =>  board1 })
+      expect(game1.other_player).to eql(alex)
+    end
+
+
+  end
 
 end
