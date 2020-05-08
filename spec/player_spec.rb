e2048b4dc00640d18bca673e2a4d5173ae5a74e1
Stephen Allard <stephencallard@gmail.com>
2020-05-08 02:49:52 -0500

    Added tests for #initialize

---
+++ spec/player_spec.rb
@@ -0,0 +1,13 @@
+require "./lib/player.rb"
+
+describe Player do
+  context "#initialize" do
+    it "Raises an exception when initialized with an empty {}" do 
+      expect{ Player.new({}) }.to raise_error(KeyError)
+    end
+
+    it "Does not raise an exception when initialized with a valid input {}" do
+      expect{ Player.new({ :name => "john", :color => "X" }) }.not_to raise_error(KeyError)
+    end
+  end
+end
