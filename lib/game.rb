98b70869f2f78be5ecd3d4d65bc1930776a14080
Stephen Allard <stephencallard@gmail.com>
2020-05-08 04:10:23 -0500

    Added #switch_players and made 'current_player' and 'other_player' attr_accessors

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,16 @@
 class Game
-  attr_reader :players, :board, :current_player, :other_player
+  attr_reader :players, :board
+  attr_accessor :current_player, :other_player
+
   def initialize(input)
     @players = input.fetch(:players)
     @board = input.fetch(:board)
     @current_player, @other_player = players.shuffle
+  end
+
+  public
+
+  def switch_players
+    self.current_player, self.other_player = other_player, current_player
   end
 end
