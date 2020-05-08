3c226906f6584f11d15abdd0c3c901197ea9dbbd
Stephen Allard <stephencallard@gmail.com>
2020-05-08 02:53:40 -0500

    Added tests for #name and #color

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,4 +10,18 @@ describe Player do
       expect{ Player.new({ :name => "john", :color => "X" }) }.not_to raise_error(KeyError)
     end
   end
+
+  context "#name" do
+    it "Returns the name of a player" do
+      player1 = Player.new({ :name => "john", :color => "X" })
+      expect(player1.name).to eql("john")
+    end
+  end
+
+  context "#color" do
+    it "Returns the color of a player" do
+      player1 = Player.new({ :name => "john", :color => "X" })
+      expect(player1.color).to eql("X")
+    end
+  end
 end
