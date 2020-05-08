44eff21bda0a44389f1ae41bab0361fdd7a04c42
Stephen Allard <stephencallard@gmail.com>
2020-05-08 03:56:15 -0500

    Added attr_readers for 'players', 'current_player', and 'other_player'

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,8 @@
 class Game
+  attr_reader :players, :current_player, :other_player
   def initialize(input)
     @players = input.fetch(:players)
     @board = input.fetch(:board)
+    @current_player, @other_player = players.shuffle
   end
 end
