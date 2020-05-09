6f4b80d364fbce66e285a11e470e2e35cc766440
Stephen Allard <stephencallard@gmail.com>
2020-05-09 03:34:38 -0500

    Added #prompt_player and #result

--- lib/game.rb
+++ lib/game.rb
@@ -1,10 +1,11 @@
 class Game
-  attr_reader :players, :board
+  attr_reader :players, :board, :console
   attr_accessor :current_player, :other_player
 
   def initialize(input)
     @players = input.fetch(:players)
     @board = input.fetch(:board)
+    @console = input.fetch(:console)
     @current_player, @other_player = players.shuffle
   end
 
@@ -12,5 +13,13 @@ class Game
 
   def switch_players
     self.current_player, self.other_player = other_player, current_player
+  end
+
+  def prompt_player
+    console.prompt("Select a column to fill.")
+  end
+
+  def result
+    return "#{current_player} is the winner!" if board.winner?
   end
 end
