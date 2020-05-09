d281b3d2d8e14e77f961e34791d22028b7f39cc8
Stephen Allard <stephencallard@gmail.com>
2020-05-09 04:29:16 -0500

    Added tests for #initialize

---
+++ spec/console_spec.rb
@@ -0,0 +1,32 @@
+require "./lib/console.rb"
+
+describe Console do
+  context "#initialize" do
+    it "Does not raise an exception when initialized with an empty {}" do
+      expect{ Console.new }.not_to raise_error(KeyError)
+    end
+
+    it "By default has input set to $stdin" do
+      console = Console.new
+      expect(console.input).to eql($stdin)
+    end
+
+    it "By default has output set to $stdout" do
+      console = Console.new
+      expect(console.output).to eql($stdout)
+    end
+
+    it "Input can be initialized with a StringIO object" do
+      console = Console.new( {:input => StringIO.new } )
+      expect(console.input).to be_a(StringIO)
+    end
+
+    it "Output can be initialized with a StringIO object" do
+      console = Console.new( {:output => StringIO.new } )
+      expect(console.output).to be_a(StringIO)
+    end
+
+  end
+
+
+end
