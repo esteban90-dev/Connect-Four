716092410a6b6603adb09f5af9c8ab886d8caedd
Stephen Allard <stephencallard@gmail.com>
2020-05-09 22:13:18 -0500

    Added tests for #invalid?

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -67,8 +67,67 @@ describe Game do
     it "Sends an output message to the console" do 
       game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
       message = "Select a column to fill."
+      allow(game1).to receive(:invalid?).and_return(false)
       expect(game1.console).to receive(:prompt).with(message)
       game1.prompt_player
+    end
+
+    it "Returns a valid input" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      message = "Select a column to fill."
+      allow(game1).to receive(:invalid?).and_return(false)
+      allow(game1.console).to receive(:prompt).with(message).and_return("5")
+      expect(game1.prompt_player).to eql(5)
+    end
+
+    it "Displays 'Invalid input.' when the input is not valid" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      message1 = "Select a column to fill."
+      message2 = "Invalid input."
+      allow(game1.console).to receive(:prompt).with(message1).and_return("10","12","5")
+      allow(game1).to receive(:invalid?).and_return(true, true, false)
+      expect(game1.console).to receive(:write).with(message2).exactly(:twice)
+      game1.prompt_player
+    end
+  end
+
+  context "#invalid?" do
+    it "Returns true when input is not a number" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      input = "a"
+      expect(game1.invalid?(input)).to eql(true)
+    end
+
+    it "Returns false when input is less than board.length, and the column isn't full" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      input = "3"
+      allow(game1.board).to receive(:length).and_return(7)
+      allow(game1.board).to receive(:column_full?).with(input.to_i).and_return(false)
+      expect(game1.invalid?(input)).to eql(false)
+    end
+
+    it "Returns true when input is greater than board.length" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      input = "10"
+      allow(game1.board).to receive(:length).and_return(7)
+      allow(game1.board).to receive(:column_full?).with(input.to_i).and_return(false)
+      expect(game1.invalid?(input)).to eql(true)
+    end
+
+    it "Returns false when input is less than board.length, and column isn't already full" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      input = "3"
+      allow(game1.board).to receive(:length).and_return(7)
+      allow(game1.board).to receive(:column_full?).with(input.to_i).and_return(false)
+      expect(game1.invalid?(input)).to eql(false)
+    end
+
+    it "Returns true when input is less than board.length, and column is already full" do
+      game1 = Game.new({ :players => players, :board =>  board1, :console => console1 })
+      input = "3"
+      allow(game1.board).to receive(:length).and_return(7)
+      allow(game1.board).to receive(:column_full?).with(input.to_i).and_return(true)
+      expect(game1.invalid?(input)).to eql(true)
     end
   end
 
